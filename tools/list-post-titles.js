caa1ebf60bffe46a8906b04629e09bf3c793b08d
Eric L. Hepperle <codewizard13@gmail.com>
2024-12-11 10:57:06 -0600

    UPDATE list-post-titles.js:  ADD commander args

--- tools/list-post-titles.js
+++ tools/list-post-titles.js
@@ -15,22 +15,35 @@
  */
 const fs = require('fs');
 const path = require('path');
+const { program } = require('commander');
+
+program
+  .option('-sd, --sourceDir <sourceDir>', 'Source Directory')
+
+program.parse(process.argv);
+
+const options = program.opts();
+console.log(`Hello, ${options.name}! You are ${options.age} years old.`);
+
 
 const start_dir = __dirname;
 console.log(`start_dir: ${start_dir}`)
 
-const folderPath = start_dir;
+console.log(`options:`, options.sourceDir)
+
+const folderPath = options.sourceDir ? options.sourceDir : start_dir;
 const files = fs.readdirSync(folderPath);
+// const files = fs.readdir(folderPath);
 
-files.forEach(file => {
-    const filePath = path.join(folderPath, file);
-    const data = fs.readFileSync(filePath, 'utf8');
+// files.forEach(file => {
+//     const filePath = path.join(folderPath, file);
+//     const data = fs.readFileSync(filePath, 'utf8');
 
-    const regex = /^# (.*)/m;
-    const match = data.match(regex);
+//     const regex = /^# (.*)/m;
+//     const match = data.match(regex);
 
-    if (match) {
-        const post_title = match[1];
-        console.log(`File: ${file} - Post Title: ${post_title}`);
-    }
-});
+//     if (match) {
+//         const post_title = match[1];
+//         console.log(`File: ${file} - Post Title: ${post_title}`);
+//     }
+// });
